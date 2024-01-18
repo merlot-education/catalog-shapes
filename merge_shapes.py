c362b4ed65045e1a990fe4c04bb8db7918eb48dd
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-01-18 14:35:12 +0100

    improve merge script

--- merge_shapes.py
+++ merge_shapes.py
@@ -1,61 +1,21 @@
 import os
-import warnings
+import rdflib
 
 shape_base_folder = os.path.join(os.path.dirname(__file__), "shacl/shapes/")
 shape_output_path = os.path.join(shape_base_folder, "mergedShapes.ttl")
 
-prefixes = set()
-shape_dict = {}
-shape_defs = []
+graph = rdflib.Graph()
 
-for root, dirs, files in os.walk(shape_base_folder, topdown=False):
+for root, _, files in os.walk(shape_base_folder, topdown=False):
     for name in files:
         file_path = os.path.join(root, name)
         if file_path == shape_output_path:
             continue
         print(file_path)
-        with open(os.path.join(root, name), "r") as f:
+        with open(file_path, "r") as f:
+            try:
+                graph.parse(file_path, format='ttl')
+            except Exception as e:
+                raise Exception("An error occurred while parsing " + str(file_path) + ": " + str(e))
 
-            blocks = []
-            block_start = 0
-            block_end = 0
-            idx = 0
-            tmp_list = []
-            lines = f.readlines()
-            for s in lines:
-                if s.startswith("@prefix"):
-                    prefixes.add(s)
-                elif s.startswith("gax-validation:") and s.endswith("a sh:NodeShape ;\n"):
-                    block_start = idx
-                elif s.endswith(".\n"):
-                    block_end = idx
-                    blocks.append(lines[block_start:block_end+1])
-                idx += 1
-
-            if not blocks:
-                continue
-
-            for shape_list in blocks:
-                shape_header = shape_list[0]
-                shape_content = "".join(shape_list[1:])
-                target_shape = shape_list[-1].strip()
-                if (shape_header in shape_dict.keys()) \
-                        and (shape_dict[shape_header]["content"] != shape_content)\
-                        and (shape_dict[shape_header]["target_shape"] == target_shape):
-                    warnings.warn(
-                        "WARNING: Redifining " + shape_header + " with different content, this might cause issues if it is not just order related")
-                    #print(file_path, shape_dict[shape_header]["file_path"], target_shape, shape_dict[shape_header]["target_shape"])
-                shape_dict[shape_header] = {
-                    "content": shape_content,
-                    "file_path": file_path,
-                    "target_shape": target_shape
-                }
-
-
-with open(shape_output_path, "w") as f:
-    f.writelines(prefixes)
-    f.write("\n")
-    for shape_header in shape_dict.keys():
-        f.write(shape_header)
-        f.write(shape_dict[shape_header]["content"])
-        f.write("\n")
+graph.serialize(shape_output_path, format='ttl')
